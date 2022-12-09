b8ea8c7f261a1d10d9bfccfb8a97a37a23191c99
Pavel Kniga <Pavel.Kniga1@kyndryl.com>
2022-12-09 18:40:40 +0300

    added usage of env variable name

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL =  "https://9.212.148.153:8443"; 
+export const BASE_URL =  window._env_.BACKEND_SERVER; 
 export const GET_DEVIATIONS = BASE_URL + '/api/deviations/zz1/policy/';
 
 export const GET_HC_CYCLES = BASE_URL + '/api/zz1/health-check-cycles';
